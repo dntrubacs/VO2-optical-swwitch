011ea77f292d4a04012264b9fbf4862a7961e012
Daniel-Iosif Trubacs (dit1u20) <dit1u20@soton.ac.uk>
2024-04-04 12:48:09 +0100

    inital commit

---
+++ agilent_connections.py
@@ -0,0 +1,37 @@
+import pyvisa
+rm = pyvisa.ResourceManager()
+import time
+import numpy as np
+import sys
+
+
+
+def write_laser_amplitude(laser_power) -> float:
+    """
+
+    :param laser_instrument:
+    :param laser_power:
+    :return: the value in mW
+    """
+    response_received = False
+    while response_received is False:
+        try:
+            laser_instrument = rm.open_resource('USB0::0x0957::0x3718::MY50701120::INSTR')
+            laser_instrument.write(f':SOURce:CHANnel1:POWer:LEVel:IMMediate:AMPLitude {laser_power} MW')
+            response = float(laser_instrument.query(':SOURce:CHANnel:POWer:LEVel:IMMediate:AMPLitude?'))
+            print(1000*response)
+            response_received = True
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+
+    return response * 1000
+
+if __name__ == '__main__':
+    # agilent laser
+
+    for power in np.arange(5, 37):
+        time_now = time.time()
+        response = write_laser_amplitude(laser_power=power)
+        print('laser power:', response, ' mW at time: ', time_now)
+        time.sleep(15)
