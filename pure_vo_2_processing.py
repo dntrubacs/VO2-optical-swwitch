690eea21ba484f3373687ac7039e234820d27d1f
Daniel-Iosif <dntrubacs@gmail.com>
2024-04-06 21:16:14 +0100

    refactore

--- pure_vo_2_processing.py
+++ pure_vo_2_processing.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from utils import load_transmitted_power
+from utils import load_transmitted_power, linear
+from scipy.optimize import curve_fit
 
 # a list of all the processed data
 input_laser_power_list = []
@@ -10,10 +11,16 @@ transmitted_laser_power_list = []
 # the cutoff data point for taking the average
 average_cutoff = 107
 
+# fit all this data to this point
+fit_1_stop = 36
+
+# fit all this data after this point
+fit_2_start = 41
+
 # the average transmitted power
 average_transmitted_power = np.zeros(shape=(average_cutoff, ))
 
-for i in [3, 4, 5, 7, 8, 9]:
+for i in [3, 4, 5, 7, 8, 9, 10, 12, 13]:
     # load the values
     input_laser_power, transmitted_laser_power = load_transmitted_power(
         file_path=f'pure_vo2_sample_{i}'
@@ -45,19 +52,38 @@ switching_region_stop = 17
 min_input_power = 5
 max_input_power = 36.8
 
+# fit the average transmitted power
+m_1, n_1 = curve_fit(
+    f=linear, xdata=input_laser_power[0:fit_1_stop],
+    ydata=average_transmitted_power[0:fit_1_stop])[0]
+m_2, n_2 = curve_fit(
+    f=linear, xdata=input_laser_power[fit_2_start:average_cutoff],
+    ydata=average_transmitted_power[fit_2_start:average_cutoff])[0]
+
+
 # plot the data
 # show all the data in the same graph
 plt.figure(figsize=(12, 8))
 for i in range(len(input_laser_power_list)):
     plt.plot(input_laser_power_list[i], transmitted_laser_power_list[i],
-             label=f'sample {i}', lw=0.7
+             label=f'sample {i+1}', lw=0.7
              )
 
 # plot the average transmitted power
 plt.plot(
     input_laser_power[0:average_cutoff], average_transmitted_power,
-    color='black', lw=3, label='average'
+    color='black', lw=3, label='averaged transmitted power',
+    linestyle='dashed'
 )
+
+# plot the fitted data
+plt.plot(input_laser_power[0:fit_1_stop],
+         m_1*input_laser_power[0:fit_1_stop]+n_1, color='red',
+         label='fitted before switching')
+plt.plot(input_laser_power[fit_2_start:average_cutoff],
+         m_2*input_laser_power[fit_2_start:average_cutoff]+n_2, color='red',
+         label='fitted after switching')
+
 
 # put limits on the data
 plt.xlim([min_input_power, max_input_power])
